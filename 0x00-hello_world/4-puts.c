0ab25f55bbd8bd00812c36442eef024086609dda
moazmuhamed <moaz.muhammed95@hotmail.com>
2023-03-16 16:20:55 -0700

    aa

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,9 +1,13 @@
 #include <stdio.h>
 
-
 /**
- * maon entry point*/
-int main (void)
+ * main -  entry point 
+ *
+ * DESCRIPTION: A C program that print with puts function 
+ *
+ * Return: Always 0 (success) 
+*/
+int main(void)
 {
 	puts("Programming is like building a multilingual puzzle");
 	return (0);
