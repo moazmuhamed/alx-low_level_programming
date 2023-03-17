d14e49e4cfce10ae482549161ff1b6488f1cad4f
moazmuhamed <moaz.muhammed95@hotmail.com>
2023-03-16 17:05:30 -0700

    siza

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -7,5 +7,9 @@
 int main(void)
 {
 	printf("size of a char: %i byte(s)\n", sizeof(char));
-	printf("size of an int: %i byte(i)\n", sizeof(int)));
-	printf("size of a long int: %
+	printf("size of an int: %i byte(s)\n", sizeof(int)));
+	printf("size of a long int: %i byte(s)\n", sizeof(long int));
+	printf("Size of a long long int: %d byte(s)\n", sizeof(long long int));
+	printf("Size of a float: %d byte(s)\n", sizeof(float));
+	return (0);
+}
