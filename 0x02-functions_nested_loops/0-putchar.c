138754c8ef5f590cfc085c861a5a5ecbb9fe52ca
moazmuhamed <moaz.muhammed95@hotmail.com>
2023-03-21 11:57:45 -0700

    moaaaz

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,22 +1,22 @@
 #include "main.h"
-	/**
-	 * main - entry point
-	 * description: 'prints _putchar to stdout'
-	 * Return: always 0
-	 */
+/**
+ * main - print _putchar
+ * is written by moaz
+ *
+ * Return: 0
+ */
 
-
-	int main(void)
-	{
-	_putchar('_');
-	_putchar('p');
-	_putchar('u');
-	_putchar('t');
-	_putchar('c');
-	_putchar('h');
-	_putchar('a');
-	_putchar('r');
-	_putchar('\n');
-	return (0);
-	}
+int main(void)
+{
+_putchar('_');
+_putchar('p');
+_putchar('u');
+_putchar('t');
+_putchar('c');
+_putchar('h');
+_putchar('a');
+_putchar('r');
+_putchar('\n');
+return (0);
+}
 
