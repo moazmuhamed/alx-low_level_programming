dfde705ad9bd75dc56bdaef81b0d51ea081c7a9c
moazmuhamed <moaz.muhammed95@hotmail.com>
2023-04-10 14:06:22 -0700

    aaa

--- 0x09-static_libraries/main.h
+++ 0x09-static_libraries/main.h
@@ -1,5 +1,5 @@
 #ifndef MAIN_H
-#define MAIN_H
+#define MAIN_H`
 
 
 
@@ -25,4 +25,4 @@ char *_strpbrk(char *s, char *accept);
 char *_strstr(char *haystack, char *needle);
 
 
-#endif /MAIN_H/
+#endif
