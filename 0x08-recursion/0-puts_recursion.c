656f1aefa73b2cf1db1cc1811f4ae7edeaf56caa
moazmuhamed <moaz.muhammed95@hotmail.com>
2023-06-07 11:46:29 -0700

    first

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -1,17 +1,17 @@
-#include <stioh.h>
-
+#include "main.h"
 /**
- *  * _puts_recursion - function like puts();
+ * _puts_recursion - function like puts();
  * @s: input
- * return 0 success
+ * Return: Always 0 (Success)
  */
 void _puts_recursion(char *s)
 {
 	if (*s)
-{
-	putchar(*s);
-	_puts_recursion(s + 1);
-}
-else
-_putchar('\n');
+	{
+		_putchar(*s);
+		_puts_recursion(s + 1);
+	}
+
+	else
+		_putchar('\n');
 }
